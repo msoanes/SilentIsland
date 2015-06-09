a8fd32b91e838205b40a01c4b8b155be68824d64
App Academy Student <student@appacademy.io>
2015-06-09 11:28:54 -0700

    Get basic sign up working

--- SilentIsland/app/controllers/application_controller.rb
+++ SilentIsland/app/controllers/application_controller.rb
@@ -2,4 +2,33 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def current_user
+    @current_user || User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login_required
+    redirect_to new_user_url unless logged_in?
+  end
+
+  def login_forbidden
+    redirect_to root_url if logged_in?
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!(user)
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
---
+++ SilentIsland/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  before_action :login_forbidden
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+end
