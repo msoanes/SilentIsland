3cd2f58112c64872231040696a256d6aa5e652ff
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-09 13:51:55 -0700

    Add songs association to User

--- SilentIsland/app/models/user.rb
+++ SilentIsland/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  has_many :songs, foreign_key: :uploader_id
+
   after_initialize :ensure_session_token
 
   attr_accessor :password
