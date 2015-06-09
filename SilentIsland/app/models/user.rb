32ada997e8ca7f2493e8921f62daba222f576205
App Academy Student <student@appacademy.io>
2015-06-09 11:12:31 -0700

    Define basic User model

---
+++ SilentIsland/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_accessor :password
+
+  def self.find_by_credentials(credentials)
+    user = self.find_by(username: credentials[:username])
+    user && user.is_password?(credentials[:password])
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    save
+    session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+end
