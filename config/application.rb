d8b2d82daa3b851b8e6a27615ed88341eedd93da
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-10 09:34:31 -0700

    Begin filepicker info

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module SilentIsland
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.filepicker_rails.api_key = ENV['FILEPICKER_API_KEY']
   end
 end
