6b9e3cd397efa9ccb7bd0d96772d3188c32e5c5c
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-10 13:11:30 -0700

    Start adding controls and display info

--- app/assets/javascripts/views/other/audio_player.js
+++ app/assets/javascripts/views/other/audio_player.js
@@ -2,24 +2,66 @@ SilentIsland.Views.AudioPlayer = Backbone.View.extend({
   template: JST['other/audio_player'],
 
   initialize: function () {
+    this.currentSong = new SilentIsland.Models.Song();
     this.listenTo(this.collection, 'play', this.switchSong);
+    setInterval(this.updateCurrentTime.bind(this), 500);
   },
 
   events: {
-    'click .play-pause': 'togglePlay',
+    'click .play-button-main': 'togglePlay',
+    'change input.seeker': 'seek',
   },
 
   render: function () {
     this.$el.html(this.template({
       currentSong: this.currentSong
     }));
+    this.$('audio').on('loadedmetadata', this.setInfo.bind(this))
+    this.$('audio').attr('src', this.currentSong.get('url'));
     return this;
   },
 
   switchSong: function (model) {
-    console.log("Changed Song");
-    console.log(model);
     this.currentSong = model;
     this.render();
+  },
+
+  togglePlay: function () {
+    var audio = this.$('audio')[0];
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  },
+
+  setInfo: function () {
+    var duration = Math.floor(this.$('audio')[0].duration);
+    var renderedDuration = this.renderTime(duration);
+    this.$('input.seeker').attr('max', duration);
+    this.$('.time-total').text(renderedDuration);
+  },
+
+  updateCurrentTime: function () {
+    var currentTime = Math.floor(this.$('audio')[0].currentTime);
+    var renderedCurrentTime = this.renderTime(currentTime);
+    this.$('input.seeker').val(currentTime);
+    this.$('.time-elapsed').text(renderedCurrentTime);
+  },
+
+  renderTime: function (time) {
+    var totalSeconds = Math.floor(time);
+    var seconds = totalSeconds % 60;
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    var minutes = Math.floor(totalSeconds / 60);
+    return '' + minutes + ':' + seconds
+  },
+
+  seek: function (event) {
+    var $input = $(event.currentTarget);
+    this.$('audio')[0].currentTime = $input.val();
+    this.updateCurrentTime();
   }
 });
