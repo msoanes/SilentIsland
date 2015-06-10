327e7460b26cfc8d6bdfa0c92598676de057a642
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-09 21:47:04 -0700

    Add automatic tag updating to song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,17 @@ class Song < ActiveRecord::Base
   belongs_to :uploader, class_name: 'User'
   has_many :taggings
   has_many :tags, through: :taggings
+
+  after_save :save_tags
+
+  def tag_labels=(labels)
+    @tag_labels = labels
+  end
+
+  def save_tags
+    tags = Tag.where('label IN (?)', @tag_labels)
+    @tag_labels -= tags.map { |tag| tag.label }
+    @tag_labels.each { |label| tags << Tag.create(label: label) }
+    self.tags = tags
+  end
 end
