7215ffb8e4d356eed470249f9b80bef68d6e3a17
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-11 14:03:22 -0700

    Add Stream collection for songs in user stream

---
+++ app/assets/javascripts/collections/stream.js
@@ -0,0 +1,18 @@
+SilentIsland.Collections.Stream = Backbone.Collection.extend({
+  url: '/api/songs/stream',
+
+  model: SilentIsland.Models.Song,
+
+  parse: function (response) {
+    var collection = this;
+    _.each(response, function (songData) {
+      var song = SilentIsland.router.songs.get(songData.id);
+      if (!song) {
+        song = new SilentIsland.Models.Song(songData);
+        SilentIsland.router.songs.add(song);
+      }
+      collection.add(song);
+    });
+    return [];
+  }
+});
