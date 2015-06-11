995689dd0567d75ae992170553da1fef1cf8ceab
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-11 00:42:10 -0700

    Add parse method to parse Tag songs

--- app/assets/javascripts/models/tag.js
+++ app/assets/javascripts/models/tag.js
@@ -1,3 +1,30 @@
 SilentIsland.Models.Tag = Backbone.Model.extend({
   urlRoot: '/api/tags',
+
+  parse: function (payload) {
+    if (payload.songs) {
+      this._addSongs(payload.songs);
+      delete payload.songs;
+    }
+    return payload;
+  },
+
+  songs: function () {
+    if (!this._songs) {
+      this._songs = new SilentIsland.Collections.Songs();
+    }
+    return this._songs;
+  },
+
+  _addSongs: function (songs) {
+    var tag = this;
+    _.each(songs, function (songData) {
+      var songModel = SilentIsland.router.songs.get(songData.id); // Law of Demeter, fix this somehow
+      if (!songModel) {
+        songModel = new SilentIsland.Models.Song(songData);
+        SilentIsland.router.songs.add(songModel);
+      }
+      tag.songs().add(songModel);
+    });
+  }
 });
