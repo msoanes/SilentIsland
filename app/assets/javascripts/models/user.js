3ce7667972e9968a42f035cd1d1649320687f750
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-11 20:27:03 -0700

    Make users Backbone model Subscribable

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,30 +1,38 @@
-SilentIsland.Models.User = Backbone.Model.extend({
-  urlRoot: '/api/users',
+SilentIsland.Models.User = Backbone.Model.extend(
+  _.extend({}, SilentIsland.Mixins.Subscribable, {
+    urlRoot: '/api/users',
 
-  parse: function (payload) {
-    if (payload.songs) {
-      this._addSongs(payload.songs);
-      delete payload.songs;
-    }
-    return payload;
-  },
+    subscribableOptions: {
+      foreignKey: "subscribable_id",
+      subscribableType: 'User'
+    },
 
-  songs: function () {
-    if (!this._songs) {
-      this._songs = new SilentIsland.Collections.Songs();
-    }
-    return this._songs;
-  },
-
-  _addSongs: function (songs) {
-    var user = this;
-    _.each(songs, function (songData) {
-      var songModel = SilentIsland.router.songs.get(songData.id); // Law of Demeter, fix this somehow
-      if (!songModel) {
-        songModel = new SilentIsland.Models.Song(songData);
-        SilentIsland.router.songs.add(songModel);
+    parse: function (payload) {
+      this.parseSubscription(payload);
+      if (payload.songs) {
+        this._addSongs(payload.songs);
+        delete payload.songs;
       }
-      user.songs().add(songModel);
-    });
-  }
-});
+      return payload;
+    },
+
+    songs: function () {
+      if (!this._songs) {
+        this._songs = new SilentIsland.Collections.Songs();
+      }
+      return this._songs;
+    },
+
+    _addSongs: function (songs) {
+      var user = this;
+      _.each(songs, function (songData) {
+        var songModel = SilentIsland.router.songs.get(songData.id); // Law of Demeter, fix this somehow
+        if (!songModel) {
+          songModel = new SilentIsland.Models.Song(songData);
+          SilentIsland.router.songs.add(songModel);
+        }
+        user.songs().add(songModel);
+      });
+    }
+  })
+);
