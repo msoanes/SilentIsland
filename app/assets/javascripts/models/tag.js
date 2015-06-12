66c9ae05a4402223b4961d26db2a1d8b6d92fc66
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-11 17:15:40 -0700

    Modify tag.js to use the mixin

--- app/assets/javascripts/models/tag.js
+++ app/assets/javascripts/models/tag.js
@@ -1,30 +1,38 @@
-SilentIsland.Models.Tag = Backbone.Model.extend({
-  urlRoot: '/api/tags',
+SilentIsland.Models.Tag = Backbone.Model.extend(
+  _.extend({}, SilentIsland.Mixins.Subscribable, {
+    urlRoot: '/api/tags',
 
-  parse: function (payload) {
-    if (payload.songs) {
-      this._addSongs(payload.songs);
-      delete payload.songs;
-    }
-    return payload;
-  },
+    subscribableOptions: {
+      foreignKey: "subscribable_id",
+      subscribableType: 'Tag'
+    },
 
-  songs: function () {
-    if (!this._songs) {
-      this._songs = new SilentIsland.Collections.Songs();
-    }
-    return this._songs;
-  },
-
-  _addSongs: function (songs) {
-    var tag = this;
-    _.each(songs, function (songData) {
-      var songModel = SilentIsland.router.songs.get(songData.id); // Law of Demeter, fix this somehow
-      if (!songModel) {
-        songModel = new SilentIsland.Models.Song(songData);
-        SilentIsland.router.songs.add(songModel);
+    parse: function (payload) {
+      this.parseSubscription(payload);
+      if (payload.songs) {
+        this._addSongs(payload.songs);
+        delete payload.songs;
       }
-      tag.songs().add(songModel);
-    });
-  }
-});
+      return payload;
+    },
+
+    songs: function () {
+      if (!this._songs) {
+        this._songs = new SilentIsland.Collections.Songs();
+      }
+      return this._songs;
+    },
+
+    _addSongs: function (songs) {
+      var tag = this;
+      _.each(songs, function (songData) {
+        var songModel = SilentIsland.router.songs.get(songData.id); // Law of Demeter, fix this somehow
+        if (!songModel) {
+          songModel = new SilentIsland.Models.Song(songData);
+          SilentIsland.router.songs.add(songModel);
+        }
+        tag.songs().add(songModel);
+      });
+    }
+  })
+);
