0db98e0f004b11598944ec97bf88aea844a3e89a
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-14 15:11:48 -0700

    Add uploader link and tag rendering on fresh page for song detail

--- app/assets/javascripts/views/songs/song_detail.js
+++ app/assets/javascripts/views/songs/song_detail.js
@@ -1,19 +1,29 @@
 SilentIsland.Views.SongDetail = Backbone.CompositeView.extend({
   template: JST['songs/detail'],
 
+  events: {
+    'click .uploader': 'visitUploader'
+  },
+
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
-    if (this.model.get('tags')) {
-      var tagView = new SilentIsland.Views.TagList({
-        collection: this.model.get('tags')
-      });
-      this.addSubview('.tags', tagView);
-    }
+    var tagView = new SilentIsland.Views.TagList({
+      collection: this.model.get('tags') || []
+    });
+    this.addSubview('.tags', tagView);
   },
 
   render: function () {
     this.$el.html(this.template({ song: this.model }));
     this.attachSubviews();
+    var tagView = this.subviews('.tags').toArray()[0]
+    tagView.collection = this.model.get('tags') || [];
+    tagView.render();
     return this;
+  },
+
+  visitUploader: function (event) {
+    var userID = $(event.currentTarget).data('id');
+    Backbone.history.navigate('/users/' + userID, { trigger: true });
   }
 })
