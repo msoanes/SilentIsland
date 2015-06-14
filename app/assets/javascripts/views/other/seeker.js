1af0f678918086ccc1fbba7e9b4716fc05babb4d
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-14 00:06:11 -0700

    Create separate seeker view

---
+++ app/assets/javascripts/views/other/seeker.js
@@ -0,0 +1,43 @@
+SilentIsland.Views.Seeker = Backbone.View.extend({
+  template: JST['other/seeker'],
+
+  tagName: 'input',
+
+  className: 'seeker',
+
+  attributes: {
+    type: "range",
+    name:"name",
+    min: "0",
+    max: "0",
+    step: "0.1",
+    value: "0",
+  },
+
+  events: {
+    'input': 'seek',
+    'mousedown': 'startSeek',
+    'mouseup': 'endSeek',
+  },
+
+  render: function () {
+    return this;
+  },
+
+  seek: function (event) {
+    this.$audio[0].currentTime = this.$el.val();
+    this.$audio[0].play();
+  },
+
+  startSeek: function () {
+    this.$el.addClass('seeking');
+  },
+
+  endSeek: function () {
+    this.$el.removeClass('seeking');
+  },
+
+  setDuration: function (duration) {
+    this.$el.attr('max', duration);
+  }
+});
