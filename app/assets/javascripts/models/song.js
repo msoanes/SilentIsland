aba47c4c7caca583cac1cfb3293d8e738a131560
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-13 20:34:05 -0700

    Add method to Song backbone model to return comma delimited list of tags

--- app/assets/javascripts/models/song.js
+++ app/assets/javascripts/models/song.js
@@ -1,3 +1,12 @@
 SilentIsland.Models.Song = Backbone.Model.extend({
-  urlRoot: '/api/songs'
+  urlRoot: '/api/songs',
+
+  commaSepTags: function () {
+    var escapedTags =  _.map(this.get('tags'), function (tag) {
+      console.log(tag);
+      return _.escape(tag.label);
+    });
+    console.log(escapedTags);
+    return escapedTags.join(',');
+  }
 });
