86993c18a4cf4126d2831545f4e6b6ae13b07c88
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-13 23:29:36 -0700

    Add tags subview to song detail view

--- app/assets/javascripts/views/songs/song_detail.js
+++ app/assets/javascripts/views/songs/song_detail.js
@@ -1,12 +1,19 @@
-SilentIsland.Views.SongDetail = Backbone.View.extend({
+SilentIsland.Views.SongDetail = Backbone.CompositeView.extend({
   template: JST['songs/detail'],
 
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
+    if (this.model.get('tags')) {
+      var tagView = new SilentIsland.Views.TagList({
+        collection: this.model.get('tags')
+      });
+      this.addSubview('.tags', tagView);
+    }
   },
 
   render: function () {
     this.$el.html(this.template({ song: this.model }));
+    this.attachSubviews();
     return this;
   }
 })
