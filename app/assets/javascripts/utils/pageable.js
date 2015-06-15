d9937d4a3c62d190c06d9644e27c0f13c387e4b5
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-15 00:34:43 -0700

    Change pageable parse to reflect responses

--- app/assets/javascripts/utils/pageable.js
+++ app/assets/javascripts/utils/pageable.js
@@ -1,5 +1,5 @@
 SilentIsland.Mixins.PageableParse = function (payload) {
-  this.page_number = parseInt(payload.page_number);
+  this.page_number = parseInt(payload.page);
   this.total_pages = parseInt(payload.total_pages);
   return payload.models;
 };
