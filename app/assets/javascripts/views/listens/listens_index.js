d111db2efc3e39fadca9a1cac1013298844ef5c8
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-15 00:35:13 -0700

    Add infinite scroll to listens index

--- app/assets/javascripts/views/listens/listens_index.js
+++ app/assets/javascripts/views/listens/listens_index.js
@@ -2,14 +2,34 @@ SilentIsland.Views.ListensIndex = Backbone.CompositeView.extend({
   template: JST['listens/index'],
 
   initialize: function () {
-    this.addListens();
     this.listenTo(this.collection, 'reset', this.addListens);
   },
 
   render: function () {
     this.$el.html(this.template());
     this.attachSubviews();
+    this.listenForScroll();
     return this;
+  },
+
+  listenForScroll: function () {
+    $(window).off('scroll');
+    var throttledCallback = _.throttle(this.nextPage.bind(this), 200);
+    $(window).on('scroll', throttledCallback);
+  },
+
+  nextPage: function () {
+    var view = this;
+    if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      console.log(view.collection.page_number);
+      console.log(view.collection.total_pages);
+      if (view.collection.page_number < view.collection.total_pages) {
+        view.collection.fetch({
+          data: {page: view.collection.page_number + 1 },
+          success: view.addListens.bind(this)
+        });
+      }
+    }
   },
 
   addListens: function () {
