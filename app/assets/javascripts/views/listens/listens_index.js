e856d22b97905552893e842109acecec2cbf4aa0
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-15 09:24:38 -0700

    Adjust listens index view to use scrollable mixin

--- app/assets/javascripts/views/listens/listens_index.js
+++ app/assets/javascripts/views/listens/listens_index.js
@@ -1,8 +1,17 @@
-SilentIsland.Views.ListensIndex = Backbone.CompositeView.extend({
+SilentIsland.Views.ListensIndex = Backbone.CompositeView.extend(
+  _.extend({}, SilentIsland.Mixins.Scrollable, {
   template: JST['listens/index'],
 
+  scrollParams: {
+    view: 'ListensIndexItem',
+    listSelector: '.listens-list',
+    itemsCallback: function () {
+      this.$('abbr.timeago').timeago();
+    }
+  },
+
   initialize: function () {
-    this.listenTo(this.collection, 'reset', this.addListens);
+    this.listenTo(this.collection, 'reset', this.addItems);
   },
 
   render: function () {
@@ -10,38 +19,5 @@ SilentIsland.Views.ListensIndex = Backbone.CompositeView.extend({
     this.attachSubviews();
     this.listenForScroll();
     return this;
-  },
-
-  listenForScroll: function () {
-    $(window).off('scroll');
-    var throttledCallback = _.throttle(this.nextPage.bind(this), 200);
-    $(window).on('scroll', throttledCallback);
-  },
-
-  nextPage: function () {
-    var view = this;
-    if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-      if (view.collection.page_number < view.collection.total_pages) {
-        view.collection.fetch({
-          data: {page: view.collection.page_number + 1 },
-          success: view.addListens.bind(this)
-        });
-      }
-    }
-  },
-
-  addListens: function () {
-    var indexView = this;
-    indexView.collection.each(function (listen) {
-      indexView.addListen(listen);
-    });
-    this.$('abbr.timeago').timeago();
-  },
-
-  addListen: function (listen) {
-    var listensIndexItem = new SilentIsland.Views.ListensIndexItem({
-      model: listen
-    });
-    this.addSubview('.listens-list', listensIndexItem);
   }
-});
+}));
