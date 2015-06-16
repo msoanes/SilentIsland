fdf2fee356ad809be8c2014e87b1d43be38bc8d8
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-16 11:12:06 -0700

    Add categories to searchbar view

--- app/assets/javascripts/views/other/searchbar.js
+++ app/assets/javascripts/views/other/searchbar.js
@@ -4,21 +4,16 @@ SilentIsland.Views.SearchBar = Backbone.CompositeView.extend({
   render: function () {
     var view = this;
     view.$el.html(view.template());
-    view.$('input').autocomplete({
+    view.$('input').categoryComplete({
       source: function(term, callback) {
-        function parse(response) {
-          callback(response.models);
-        }
-        $.getJSON('api/tags', {
-          q: term.term,
-          per: 10}, parse);
+        $.getJSON('api/searches', { q: term.term }, callback);
       },
 
-      minLength: 2,
+      minLength: 1,
 
       select: function (event, ui) {
         var tagId = ui.item.id;
-        Backbone.history.navigate('/tags/' + ui.item.id, {trigger: true});
+        Backbone.history.navigate(ui.item.category + '/' + ui.item.id, {trigger: true});
         $(this).val('');
         return false;
       }
