3ec2d9498ead2e1da56faf2fe182986a023bf143
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-16 14:59:08 -0700

    Render view after being placed into page in swapview

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -93,7 +93,7 @@ SilentIsland.Routers.Router = Backbone.Router.extend({
 
   _swapView: function (view) {
     this._currentView && this._currentView.remove();
-    this._currentView = view;
-    this.$rootEl.html(view.render().$el);
+    this.$rootEl.html(view.$el);
+    this._currentView = view.render();
   }
 });
