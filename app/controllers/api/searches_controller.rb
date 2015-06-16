b992085484ed05610bbeaf3813f57d837f2f3a44
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-16 11:23:25 -0700

    Decrease limit to three and make search case-insensitive

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,9 +1,9 @@
 module Api
   class SearchesController < ApiController
     def index
-      @songs = Song.where("title LIKE ?", "%#{params[:q]}%").limit(5)
-      @tags = Tag.where("label LIKE ?", "%#{params[:q]}%").limit(5)
-      @users = User.where("username LIKE ?", "%#{params[:q]}%").limit(5)
+      @songs = Song.where("title ILIKE ?", "%#{params[:q]}%").limit(3)
+      @tags = Tag.where("label ILIKE ?", "%#{params[:q]}%").limit(3)
+      @users = User.where("username ILIKE ?", "%#{params[:q]}%").limit(3)
     end
   end
 end
