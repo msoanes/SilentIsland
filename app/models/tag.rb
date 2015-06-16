57096e212a11d28ac18b0ab9f45bd821939beb00
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-16 14:27:55 -0700

    Downcase and filter special chars from tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,14 @@ class Tag < ActiveRecord::Base
 
   has_many :taggings
   has_many :songs, through: :taggings
+
+  after_initialize :filter_label!
+
+  def self.filter_label(string)
+    string.downcase.gsub(/[^a-z0-9& ]+/, '')
+  end
+
+  def filter_label!
+    self.label = Tag.filter_label(label)
+  end
 end
